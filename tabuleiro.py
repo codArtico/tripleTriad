d7d11e4bd6eca9d0b33677515ecd495318020b1f
Raul Sousa <raulsousadesigner@gmail.com>
2024-09-19 22:32:39 -0300

    Consertando e criando métodos de Tabuleiro

--- tabuleiro.py
+++ tabuleiro.py
@@ -1,10 +1,18 @@
 class Tabuleiro:
     def __init__(self):
         self.slots = [None,None,None],[None,None,None],[None,None,None]
+        self.cartasColocadas = 0
 
     def colocarCarta(self,linha,coluna,carta):
         if self.slots[linha][coluna] == None:
             self.slots[linha][coluna] = carta
+            cartasColocadas +=1
+            return True
         else:
-            while(self.slots[linha][coluna] != None):
-                print("Jogada inválida, insira uma posição dísponível!")
+            print("Jogada inválida, insira uma posição dísponível!")
+            return False
+
+    def tabuleiroCheio(self): #Não é necessário passar parâmetro (Self == This)
+        if (self.cartasColocadas == 9):
+            return True
+        return False
