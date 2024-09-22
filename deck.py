0a65bff961735c622a38670e6aff4ce36b33dd5e
Raul Sousa <raulsousadesigner@gmail.com>
2024-09-22 14:23:28 -0300

    Criação da Lógica de captura (normal e Plus)

--- deck.py
+++ deck.py
@@ -1,8 +1,8 @@
 class Deck:
 
     def __init__(self):
-        deck = []
-        numCartas = 0
+        self.deck = []
+        self.numCartas = 0
 
     def addCarta(self,carta):
         self.deck[self.numCartas] = carta
--- tabuleiro.py
+++ tabuleiro.py
@@ -16,3 +16,55 @@ class Tabuleiro:
         if (self.cartasColocadas == 9):
             return True
         return False
+    
+    def verificarVizinhas (self,linha,coluna,carta):
+        direcoes = {
+            'cima': (-1, 0),
+            'baixo': (1, 0),
+            'esquerda': (0, -1),
+            'direita': (0, 1)
+        }
+
+        somas = {
+
+        }
+
+        cartasAdj = {
+
+        }
+
+        oposto = {
+            'cima':'baixo', 
+            'baixo':'cima', 
+            'esquerda':'direita', 
+            'direita':'esquerda'
+            }
+            
+            
+        
+        for direcao, (dx,dy) in direcoes.items:
+            ax,ay = linha+dx,coluna+dy
+            
+            if 0 <= ax < 3 and 0 <= ay < 3 and self.slots[ax][ay]:
+                cartaAdjacente = self.slots[ax][ay]
+                
+                valorAtual = carta.valores[direcao]
+                valorAdjacente = cartaAdjacente.valores[oposto[direcao]]
+
+                if valorAtual>valorAdjacente:
+                    cartaAdjacente.dono = carta.dono
+                    print(f'Carta na posição {ax},{ay} capturada por {carta.dono} pela regra padrão!')
+                
+                soma = valorAtual+valorAdjacente
+                somas[direcao] = soma
+                cartasAdj[direcao] = cartaAdjacente
+        
+        for direcao1, soma1 in direcoes.items:
+            for direcao2, soma2 in direcoes.items:
+                if direcao1 != direcao2 and soma1 == soma2:
+                    cartaAdj1 = cartasAdj [direcao1]
+                    cartaAdj2 = cartaAdj2 [direcao2]
+                    cartaAdj1.dono = carta.dono
+                    cartaAdj2.dono = carta.dono
+                    print(f'Capturou as cartas em {direcao1} e {direcao2} pela regra Plus!')
+
